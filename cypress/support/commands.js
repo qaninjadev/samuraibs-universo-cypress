f53fe9326aaf74f4e49d95bcaffc17bdfc677e88
Papito <p4p1to@outlook.com>
2022-04-07 01:44:56 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -78,7 +78,7 @@ Cypress.Commands.add('createAppointment', function (hour) {
 
     Cypress.env('appointmentDate', now)
    
-    const date = moment(now).format(`YYYY-MM-DD ${hour}:00`)
+    const date = moment(now).utcOffset('-0300').format(`YYYY-MM-DD ${hour}:00`)
     
     const payload = {
         provider_id: Cypress.env('providerId'),
