470b28493319c4b3e54b294c76b34547ded0e649
Papito <p4p1to@outlook.com>
2022-04-08 10:04:03 -0300

    Update index.js

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -3,7 +3,7 @@ import { el } from './elements'
 
 class Toast {
     shouldHaveText(expectText) {
-        cy.get(el.toast, { timeout: 10000 })
+        cy.get(el.toast, { timeout: 30000 })
             .should('be.visible')
             .should('have.css', 'opacity', '1', { timeout: 30000 })
             .find('p')
