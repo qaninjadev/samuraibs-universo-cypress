f039bd10359ce7cc03ee09a84cab0614b517ab32
Fernando Papito <papitorocks@gmail.com>
2022-04-08 00:01:22 -0300

    timeouts do caralho

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -5,7 +5,7 @@ class Toast {
     shouldHaveText(expectText) {
         cy.get(el.toast, { timeout: 10000 })
             .should('be.visible')
-            .should('have.css', 'opacity', '1', { timeout: 15000 })
+            .should('have.css', 'opacity', '1', { timeout: 30000 })
             .find('p')
             .should('have.text', expectText)
     }
